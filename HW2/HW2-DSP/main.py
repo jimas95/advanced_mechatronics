16ef236e3285e5bc5b71f679ad080f620741b844
jimas95 <jimas95@hotmail.com>
2021-04-06 23:32:07 -0500

    working on Q7

--- HW2/HW2-DSP/main.py
+++ HW2/HW2-DSP/main.py
@@ -1,10 +1,13 @@
 import my_functions
 from my_functions import get_data,fft,subplot
 from my_functions import plot_data,plot_data2
-
+import filter_coefficients
+buffer_size = 250
+file_name = "sigA.csv" 
+A = 0.005
+B = 1.0 - A
 
 def low_pass(time,data):
-    buffer_size = 300
     buffer = []
     new_data = []
     for i in range(buffer_size):
@@ -13,18 +16,30 @@ def low_pass(time,data):
     for val in data:
         buffer.append(val)
         buffer.pop(0)
-        # print(len(buffer))
         new_val = sum(buffer)/buffer_size
         new_data.append(new_val)
     
     return new_data
 
+def IIR_filter(time,data):
+
+    filter = [data[0]]
+    for i in range(1,len(data)):
+        new_val = A*data[i] + B*filter[-1]
+        filter.append(new_val)
+    return filter
+
+
+def FIR_filter(time,data):
+    h = filter_coefficients.get_h("A")
+    print(h)
+
 def main():
-    title = " makis"
-    file_name = "sigA.csv" 
+    title = "A:" + str(A) + " B:" + str(B)  + " Filter IIR Signal :" + file_name
     time,data = get_data(file_name)
     dt = len(time)/time[-1]
-    filter_data = low_pass(time,data)
+    filter_data = IIR_filter(time,data)
+    FIR_filter(time,data)
     freq,fft_signal = fft(time, data, dt)
     freqF,fft_signalF = fft(time, filter_data, dt)
     subplot(time,data,filter_data,freq,fft_signal,freqF,fft_signalF,title)
